d2694f820705b8f900b084f746b43da179714564
Yoshi Nagasaki <yn253@cornell.edu>
2025-03-11 22:24:25 -0400

    Separate create modal elements from edit goal modal elements

--- app/ui/formModal.tsx
+++ app/ui/formModal.tsx
@@ -11,55 +11,85 @@ export function InputFormModal({
   editGoalData?: Goal;
   postSubmitCallback?: () => void;
 }) {
-  const [activeTab, setActiveTab] = useState(!editGoalData ? 0 : 1);
-
   return (
     <div className="fixed inset-x-0 bottom-8 z-20 mx-4 rounded-lg border bg-white p-6 pt-3 text-gray-700 shadow-md md:mx-12">
-      <div className="flex flex-wrap">
-        <div className="w-full border-b pr-6 pt-0 md:w-1/4 md:border-0 md:pt-3">
-          {!editGoalData && (
-            <div
-              className={clsx(
-                'inline-block cursor-pointer p-2 pl-0 pr-3 md:block md:p-3',
-                {
-                  'font-bold': activeTab === 0,
-                },
-              )}
-              onClick={() => setActiveTab(0)}
-            >
-              Add Options
-            </div>
+      {!editGoalData && (
+        <CreateFormModal postSubmitCallback={postSubmitCallback} />
+      )}
+      {editGoalData && (
+        <EditGoalFormModal
+          editGoalData={editGoalData}
+          postSubmitCallback={postSubmitCallback}
+        />
+      )}
+    </div>
+  );
+}
+
+function CreateFormModal({
+  postSubmitCallback,
+}: {
+  postSubmitCallback?: () => void;
+}) {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <div className="flex flex-wrap">
+      <div className="w-full border-b pr-6 pt-0 md:w-1/4 md:border-0 md:pt-3">
+        <div
+          className={clsx(
+            'inline-block cursor-pointer p-2 pl-0 pr-3 md:block md:p-3',
+            {
+              'font-bold': activeTab === 0,
+            },
           )}
-          {!editGoalData && (
-            <div
-              className={clsx(
-                'inline-block cursor-pointer p-2 pl-3 md:block md:border-t md:p-3',
-                {
-                  'font-bold': activeTab === 1,
-                },
-              )}
-              onClick={() => setActiveTab(1)}
-            >
-              Add Goals
-            </div>
-          )}
-          {!!editGoalData && (
-            <div className="inline-block cursor-pointer p-2 pl-0 font-bold md:block md:p-3">
-              Edit Goal
-            </div>
-          )}
+          onClick={() => setActiveTab(0)}
+        >
+          Add Options
         </div>
-        <div className="w-full justify-center md:w-3/4">
-          {activeTab === 0 && (
-            <OptionForm postSubmitCallback={postSubmitCallback} />
+        <div
+          className={clsx(
+            'inline-block cursor-pointer p-2 pl-3 md:block md:border-t md:p-3',
+            {
+              'font-bold': activeTab === 1,
+            },
           )}
-          {activeTab === 1 && (
-            <GoalForm
-              editGoalData={editGoalData}
-              postSubmitCallback={postSubmitCallback}
-            />
-          )}
+          onClick={() => setActiveTab(1)}
+        >
+          Add Goals
         </div>
+      </div>
+      <div className="w-full justify-center md:w-3/4">
+        {activeTab === 0 && (
+          <OptionForm postSubmitCallback={postSubmitCallback} />
+        )}
+        {activeTab === 1 && (
+          <GoalForm postSubmitCallback={postSubmitCallback} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+function EditGoalFormModal({
+  editGoalData,
+  postSubmitCallback,
+}: {
+  editGoalData: Goal;
+  postSubmitCallback?: () => void;
+}) {
+  return (
+    <div className="flex flex-wrap">
+      <div className="w-full border-b pr-6 pt-0 md:w-1/4 md:border-0 md:pt-3">
+        <div className="inline-block cursor-pointer p-2 pl-0 font-bold md:block md:p-3">
+          Edit Goal
+        </div>
+      </div>
+      <div className="w-full justify-center md:w-3/4">
+        <GoalForm
+          editGoalData={editGoalData}
+          postSubmitCallback={postSubmitCallback}
+        />
       </div>
     </div>
   );
