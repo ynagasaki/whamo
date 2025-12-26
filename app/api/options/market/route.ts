b8ce844ae6dd95335d0ed8666453c5e1df2c8fb9
Yoshi Nagasaki <yn253@cornell.edu>
2025-12-26 16:03:36 -0500

    feat: add option strikes in stock info card

--- app/api/options/market/route.ts
+++ app/api/options/market/route.ts
@@ -3,12 +3,18 @@ import { fetchOpenOptions, fetchStockInfo } from '@/app/lib/data';
 export async function GET(request: Request): Promise<Response> {
   const params = new URL(request.url).searchParams;
   const dataType = params.get('dtype');
+  const include = params.getAll('inc');
 
   if (dataType !== 'stock') {
     return Response.json({ stocks: [] });
   }
 
   const stockData = await fetchStockInfo();
+
+  if (include?.includes('option')) {
+    const options = await fetchOpenOptions();
+    return Response.json({ options, stocks: stockData });
+  }
 
   return Response.json({ stocks: stockData });
 }
--- app/ui/tickerBar.tsx
+++ app/ui/tickerBar.tsx
@@ -1,16 +1,21 @@
 import useSWR from 'swr';
 import { fetcher } from '@/app/lib/util';
-import { StockInfo } from '@/app/lib/model';
+import { Option, StockInfo } from '@/app/lib/model';
 import dayjs from 'dayjs';
+import clsx from 'clsx';
 
 export function TickerBar() {
-  const { data, error } = useSWR(`/api/options/market?dtype=stock`, fetcher);
+  const { data, error } = useSWR(
+    `/api/options/market?dtype=stock&inc=option`,
+    fetcher,
+  );
 
   if (!data || error) {
     return <span className="hidden"></span>;
   }
 
   const tickers: StockInfo[] = data.stocks;
+  const options: Option[] = data.options;
 
   if (tickers.length === 0) {
     return <span className="hidden"></span>;
@@ -21,7 +26,11 @@ export function TickerBar() {
       <>
         {tickers.map((t) => {
           return (
-            <TickerCard key={`ticker-card-${t.symbol}`} ticker={t}></TickerCard>
+            <TickerCard
+              key={`ticker-card-${t.symbol}`}
+              ticker={t}
+              options={options.filter((o) => o.symbol === t.symbol)}
+            ></TickerCard>
           );
         })}
       </>
@@ -29,18 +38,57 @@ export function TickerBar() {
   );
 }
 
-function TickerCard({ ticker }: { ticker: StockInfo }) {
+function TickerCard({
+  ticker,
+  options,
+}: {
+  ticker: StockInfo;
+  options: Option[];
+}) {
+  const sortedOptions = options.toSorted((o1, o2) => {
+    if (o1.otype === o2.otype) {
+      return o1.strike - o2.strike;
+    }
+    if (o1.otype === 'CALL') {
+      return -1;
+    }
+    return 1;
+  });
+
   return (
     <div className="w-1/2 pr-2 md:w-1/4">
       <div className="flex rounded-md bg-white p-3">
         <div className="w-1/2">
           <span className="block text-sm">{ticker.symbol}</span>
-          <span className="block text-xl">{ticker.price}</span>
+          <span className="block text-xl">
+            {Math.round(ticker.price * 100) / 100}
+          </span>
           <span className="block text-xs text-gray-400">
             {dayjs(ticker.last_updated).format('MM-DD HH:mm')}
           </span>
         </div>
-        <div className="w-1/2">&nbsp;</div>
+        <div className="h-16 w-1/2 overflow-y-auto">
+          <>
+            {sortedOptions.map((option) => {
+              const itm =
+                (option.otype === 'CALL' && ticker.price >= option.strike) ||
+                (option.otype === 'PUT' && ticker.price <= option.strike);
+
+              return (
+                <span
+                  className={clsx('block px-2 text-sm', {
+                    'bg-yellow-200 text-yellow-600': itm,
+                  })}
+                >
+                  <span className="text-xs font-bold">
+                    {option.otype.charAt(0)}-
+                  </span>
+                  {option.strike}
+                </span>
+              );
+            })}
+          </>
+        </div>
       </div>
     </div>
   );
